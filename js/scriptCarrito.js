66b3918f43621c4f88a92598d9882c1d6f575326
Robert Flores <rrfloresgonzalez@gmail.com>
2021-03-21 19:59:29 -0300

    se arregló la tabla del carrito que se sobreescrbía con cada producto agregado

--- js/scriptCarrito.js
+++ js/scriptCarrito.js
@@ -3,12 +3,17 @@ console.log(carrito2);
 
 function agregarAlCarrito(carro){
     let tablaCompras = document.getElementById("carroCompras");
-    let tablaProducto = document.createElement("tr");        
+    let tablaProducto = document.createElement("table");
+    tablaProducto.innerHTML = ` <tr>
+                                 <th colspan="2">Producto</th>
+                                 <th>Precio</th>
+                                </tr>`        
     for (const elemento of carro){
-    tablaProducto.innerHTML = ` <td> <img src='${elemento.img}' class='imgCarrito'></td>
+    tablaProducto.innerHTML += `<tr><td> <img src='${elemento.img}' class='imgCarrito'></td>
                                 <td> ${elemento.nombre} </td>
-                                <td> ${elemento.precio} </td> `
-                }    
+                                <td> ${elemento.precio} </td></tr>`
+                }  
+    tablaProducto.classList.add('tablaDeCompras');               
     tablaCompras.appendChild(tablaProducto);
 }
   agregarAlCarrito(carrito2);
