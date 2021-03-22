610329c270ca29debf9d2ece1bb3d1ed2d7d863b
Robert Flores <rrfloresgonzalez@gmail.com>
2021-03-21 23:28:24 -0300

    se logró solucionar los problemas del borrado storage y el table de carrito

--- js/scriptIndex.js
+++ js/scriptIndex.js
@@ -6,37 +6,41 @@ function Producto(nombre, serial, precio, img) {
 }
 
 const productos = [];
-let productosElegidos = [];
-let carro = [];
-let tienda = document.getElementById("seleccionProductos");
-let listado = document.createElement("div");
-
 productos.push(new Producto("stickers", 1, 100, "img/sticker.jpg"));
 productos.push(new Producto("remeras", 2, 500, "img/remera.jpg"));
 productos.push(new Producto("tazas", 3, 250, "img/taza.jpg"));
 productos.push(new Producto("cuadros", 4, 1000, "img/cuadro.jpg"));
 
+let productosElegidos = [];
+let tienda = document.getElementById("seleccionProductos");
+let listado = document.createElement("div");
+
 const boton = document.getElementById("despliego");
 boton.addEventListener('click', opciones);
 
-// función que transforma a JSON un array de objetos
-function carroJSON (dato){
-    let carritoJSON = JSON.stringify(dato);
-    console.warn(carritoJSON);
-    localStorage.setItem('carrito', carritoJSON);
+const botonR= document.getElementById("recoge");
+botonR.addEventListener('click', eliminarMenu);
+
+// función que agrega productos al array del localstorage
+function carroJSON (dato){ 
+    //si el carrito de storage no existe crea el primer elemento         
+    if (!localStorage.getItem('carrito')){ 
+        let carritoJSON = JSON.stringify(productosElegidos);
+        localStorage.setItem('carrito', carritoJSON); 
+    }
+    //si ya hay un valor en el carrito le hace un psuh al array del storage
+    else {
+        let validarCarrito = JSON.parse(localStorage.getItem('carrito'));       
+        validarCarrito.push(dato);  
+        localStorage.setItem('carrito', JSON.stringify(validarCarrito));     
+    }
 }
 // función llamada en los eventos de "botonCarrito" que crea el array de los productos elegidos al presionar el botón de comprar
-function seleccionarProducto (e){
-    
+function seleccionarProducto (e){    
     let productoElegido = productos.find(obj => obj.serial == e.target.id);
-    console.log(productoElegido);
     productosElegidos.push(productoElegido);
-    console.log(productosElegidos);
-    // carroJSON(productosElegidos);
-    carro = productosElegidos;
-    carroJSON(carro);
+    carroJSON(productoElegido);
    }
-
 //función asociada al evento que despliega por el DOM las opciones de compra 
 function opciones(e) {
     let menu = '';
@@ -63,20 +67,15 @@ function opciones(e) {
         for (const botonC of botonesCarrito){
         botonC.addEventListener('click' , seleccionarProducto);
         }       
-    }
-   
-    const botonR= document.getElementById("recoge");
-    botonR.addEventListener('click', eliminarMenu);
-
- function eliminarMenu(e){
-    tienda.removeChild(listado);
-    tienda.value = true;
+    }  
+//Función para replegar el menú en el index 
+function eliminarMenu(e){
+tienda.removeChild(listado);
+tienda.value = true;
  }
 
 
-
-//  ahora que tengo el JSON del carrito en el storage, probar hacer un parse a ese array para generar el HTML del carrito
-// hacer un JS para carrito específicamente con las funciones para extraer la info del storage y generar el nuevo DOM
-// a su vez probar el método que usé para eleminar el menú desplegado, pero para eliminar items del carrito con un botón
+//Hacer un botón que limpie el storage para que no se embasure el carrito
+//probar el método que usé para eleminar el menú desplegado, pero para eliminar items del carrito con un botón
 // X o algo por el estilo, probar con JQUERY para a su vez agregar los elementos al carrito al revés con "prepend"
 // en lugar del append 
