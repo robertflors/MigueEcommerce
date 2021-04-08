87871c1eb5c44a3367bdcae9b7d8fa15ee22c53f
Robert Flores <rrfloresgonzalez@gmail.com>
2021-04-07 23:27:55 -0300

    se agregó la función de elegir tu pokemon desde la pokedex

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,17 +1,21 @@
 
 const apiPokemon = "https://pokeapi.co/api/v2/pokedex/2/"
 $(document).ready(function () {
-    console.log("dom listo");
     $.get(apiPokemon, function(poke) {
         console.log(poke.pokemon_entries);
         $("#pokedex").empty();
         for (const pokemon of poke.pokemon_entries) {
-            $("#pokedex").append(`<option>${pokemon.pokemon_species.name}</option>`);            
+            $("#pokedex").append(`<option value="${pokemon.pokemon_species.name}">${pokemon.pokemon_species.name}</option>`);            
         }
     });
 });
 
 $("#pokedex").change(function (e) { 
-    console.log(this.value); 
-    
+    $("#imagenPokemon").empty(); 
+    const pokemonElegido = `https://pokeapi.co/api/v2/pokemon/${this.value}`;
+    console.log(pokemonElegido); 
+    $.get(pokemonElegido, function (poke) {
+        $("#imagenPokemon").append(`<img src="${poke.sprites.front_default}">
+                                    <img src="${poke.sprites.back_default}">  `);
+    });
 });
